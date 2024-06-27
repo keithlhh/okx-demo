4eda98828cc95c27dbacd7cbd84eb2e6ea35d0a3
ex_lihh14 <ex_lihh14@partner.midea.com>
2024-06-27 22:55:31 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,65 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Table } from 'antd';
-import dayjs from 'dayjs';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Table } from "antd";
+import dayjs from "dayjs";
 function App() {
   const columns = [
     {
-      title: '用户',
-      dataIndex: 'nickName',
+      title: "用户",
+      dataIndex: "nickName",
     },
     {
-      title: '币种',
-      dataIndex: 'baseName',
+      title: "币种",
+      dataIndex: "baseName",
     },
     {
-      title: '类型',
-      dataIndex: 'side',
+      title: "类型",
+      dataIndex: "side",
       render: (_, { posSide, side }) => {
-        if (posSide === 'long' && side === 'buy') {
-          return <div style={{ color: 'green' }}>做多</div>
-        } else if (posSide === 'long' && side === 'sell') {
-          return <div style={{ color: 'blue' }}>平多</div>
-        } else if (posSide === 'short' && side === 'sell') {
-          return <div style={{ color: 'green' }}>做空</div>
-        } else if (posSide === 'short' && side === 'buy') {
-          return <div style={{ color: 'blue' }}>平空</div>
+        if (posSide === "long" && side === "buy") {
+          return <div style={{ color: "green" }}>做多</div>;
+        } else if (posSide === "long" && side === "sell") {
+          return <div style={{ color: "blue" }}>平多</div>;
+        } else if (posSide === "short" && side === "sell") {
+          return <div style={{ color: "green" }}>做空</div>;
+        } else if (posSide === "short" && side === "buy") {
+          return <div style={{ color: "blue" }}>平空</div>;
         }
-      }
+      },
     },
     {
-      title: '均价',
-      dataIndex: 'avgPx',
+      title: "均价",
+      dataIndex: "avgPx",
     },
     {
-      title: '时间',
-      dataIndex: 'cTime',
+      title: "时间",
+      dataIndex: "cTime",
       render: (_) => {
-        return new Date(Number(_)).toLocaleDateString() + ' ' + new Date(Number(_)).toLocaleTimeString()
-      }
-    }
-  ]
+        return (
+          new Date(Number(_)).toLocaleDateString() +
+          " " +
+          new Date(Number(_)).toLocaleTimeString()
+        );
+      },
+    },
+  ];
   const [recordData, setRecordData] = useState([]);
   const [curData, setCurData] = useState([]);
   useEffect(() => {
-    axios.get('https://okx-info-service.vercel.app/api/trade-records?limit=100').then((res) => {
-      console.log(res?.data?.data, '238938988')
-      setRecordData(res?.data?.data)
-    })
-    axios.get('https://okx-info-service.vercel.app/api/positions').then((res) => {
-      console.log(res?.data?.data, 'kkkkkkk')
-      setCurData(res?.data?.data)
-    })
-  }, [])
+    axios
+      .get("https://okx-info-service.vercel.app/api/trade-records?limit=100")
+      .then((res) => {
+        console.log(res?.data?.data, "238938988");
+        setRecordData(res?.data?.data);
+      });
+    axios
+      .get("https://okx-info-service.vercel.app/api/positions")
+      .then((res) => {
+        console.log(res?.data?.data, "kkkkkkk");
+        setCurData(res?.data?.data);
+      });
+  }, []);
   return (
     <div className="App">
       <h3>当前持仓</h3>
-      <Table empty={'hello'} dataSource={curData} />
+      <Table
+        pagination={{
+          defaultPageSize: 100,
+          showSizeChanger: true,
+          pageSizeOptions: ["10", "20", "50", "100"],
+        }}
+        empty={"hello"}
+        dataSource={curData}
+      />
       <h3>历史记录</h3>
-      <Table columns={columns} dataSource={recordData} />
-      <div style={{paddingBottom: 100}}>{JSON.stringify(curData)}</div>
+      <Table
+        pagination={{
+          defaultPageSize: 100,
+          showSizeChanger: true,
+          pageSizeOptions: ["10", "20", "50", "100"],
+        }}
+        columns={columns}
+        dataSource={recordData}
+      />
+      <div style={{ paddingBottom: 100 }}>{JSON.stringify(curData)}</div>
     </div>
   );
 }
